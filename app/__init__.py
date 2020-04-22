2bedaa0b95b426e17512ddcc5e4d43e58fa9d395
Kartik Patidar <kartikpatidar815@gmail.com>
2020-04-22 17:03:54 +0530

    User login and logout features and registration page for new users

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config) # linking config file to our app
 db = SQLAlchemy(app) # represents database
 migrate = Migrate(app, db) # represents migration engine
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
