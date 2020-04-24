eee2b56de66abd4053b1f548f11795ad18f7e44e
Kartik Patidar <kartikpatidar815@gmail.com>
2020-04-24 21:44:46 +0530

    send mail when error occurs(500) and maintaining a log file for server

--- config.py
+++ config.py
@@ -9,4 +9,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' +os.path.join(basedir, 'app.db')
     # so that flask-sqlalchemy do not send signal when changes are about to be made in database.
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    #TO SEND EMAIL:
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['admin@example.com']
     
